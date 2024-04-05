e152d66dcab5e0c1f725a22ccfbb3fb485b8f1c3
John Corser <johnpc@umich.edu>
2024-04-05 17:35:18 -0400

    temporarily remove web-push functionality

--- helpers/notify.ts
+++ helpers/notify.ts
@@ -1,4 +1,4 @@
-import webpush from "web-push";
+// import webpush from "web-push";
 import { env } from "process";
 import { deleteNotification, listNotifications } from "../data/entities";
 
@@ -18,31 +18,31 @@ export const notify = async (body: {
     return;
   }
   // Web Push
-  const notificationRecords = await listNotifications();
+  // const notificationRecords = await listNotifications();
 
-  webpush.setVapidDetails(
-    "mailto:support@arboreats.app",
-    env.WEB_NOTIFICATION_PUBLIC_KEY!,
-    env.WEB_NOTIFICATION_PRIVATE_KEY!
-  );
-  const sendAllNotifications = notificationRecords.map(async (notification) => {
-    try {
-      await webpush.sendNotification(
-        {
-          endpoint: notification.endpoint,
-          keys: {
-            p256dh: notification.p256dh,
-            auth: notification.auth,
-          },
-        },
-        JSON.stringify({ message: "New Eat Available!" })
-      );
-    } catch (e) {
-      console.error("Removing failed notification record");
-      console.error(e);
-      await deleteNotification(notification);
-    }
-  });
-  console.log(`sending ${sendAllNotifications.length} notifications`);
-  await Promise.all(sendAllNotifications);
+  // webpush.setVapidDetails(
+  //   "mailto:support@arboreats.app",
+  //   env.WEB_NOTIFICATION_PUBLIC_KEY!,
+  //   env.WEB_NOTIFICATION_PRIVATE_KEY!
+  // );
+  // const sendAllNotifications = notificationRecords.map(async (notification) => {
+  //   try {
+  //     await webpush.sendNotification(
+  //       {
+  //         endpoint: notification.endpoint,
+  //         keys: {
+  //           p256dh: notification.p256dh,
+  //           auth: notification.auth,
+  //         },
+  //       },
+  //       JSON.stringify({ message: "New Eat Available!" })
+  //     );
+  //   } catch (e) {
+  //     console.error("Removing failed notification record");
+  //     console.error(e);
+  //     await deleteNotification(notification);
+  //   }
+  // });
+  // console.log(`sending ${sendAllNotifications.length} notifications`);
+  // await Promise.all(sendAllNotifications);
 };
